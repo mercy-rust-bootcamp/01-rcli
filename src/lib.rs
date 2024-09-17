dd4d45f948692142771def7adb3082f2f17182d1
tyrxuan <1443424326@qq.com>
2024-09-18 00:48:03 +0800

    feature: add password generator

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ mod cli;
 mod process;
 
 pub use cli::{Cli, SubCommand};
-pub use process::process_csv;
+pub use process::*;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // rcli csv -i input.json -o output.json --header -d ','
 
 use clap::Parser;
-use rcli::{process_csv, Cli, SubCommand};
+use rcli::{process_csv, process_genpass, Cli, SubCommand};
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
@@ -14,6 +14,15 @@ fn main() -> anyhow::Result<()> {
             };
 
             process_csv(&opts.input, output, opts.format)?;
+        }
+        SubCommand::GenPass(opts) => {
+            process_genpass(
+                opts.length,
+                opts.no_uppercase,
+                opts.no_lowercase,
+                opts.no_number,
+                opts.no_symbol,
+            )?;
         }
     }
 
